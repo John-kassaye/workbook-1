b77264727469967654a59ee4dd1a2dd06e981ca9
Yohannes Kassaye <jmule1567@gmail.com>
2025-04-09 19:21:31 -0700

    second update

---
+++ first-maven-project/src/main/java/app.java
@@ -0,0 +1,31 @@
+public class app {
+
+    public static void main(String[] args) {
+        Long vehicle_number= 1223435L;
+        String vehicle_model= "tesla";
+        String vehicle_color= "white";
+        int price= 25000;
+        char quality= 'A';
+        Long phone_number= 4253456666L;
+        int zip_code= 98123;
+
+        int x = 12;
+        int y = 7;
+        int z = 3;
+        int a = 10;
+        int b = 4;
+        int c = 2;
+        int d = 5;
+        int e = 20;
+        int f = 4;
+        int g = 10;
+        int h = 3;
+        System.out.println(y+z);
+        System.out.println(a-b);
+        System.out.println(c*d);
+        System.out.println(e/f);
+        System.out.println(g%h);
+        //System.out.println("Hello, world!");
+        //System.out.println("the vehicle color is " + vehicle_color);
+    }
+}
