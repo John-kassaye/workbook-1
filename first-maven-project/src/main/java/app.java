5a8caa908502f6119cc45a2dacb1a9651b028c85
John-kassaye <jmule1567@gmail.com>
2025-04-09 19:31:24 -0700

    Update app.java

--- first-maven-project/src/main/java/app.java
+++ first-maven-project/src/main/java/app.java
@@ -8,23 +8,38 @@ public class app {
         char quality= 'A';
         Long phone_number= 4253456666L;
         int zip_code= 98123;
-
         int x = 12;
+        
         int y = 7;
         int z = 3;
+        int result
+            result = y + z
+            System.out.println(result);
+
         int a = 10;
         int b = 4;
+             result = a - b
+             System.out.println(result);
+
         int c = 2;
         int d = 5;
+             result = c * d
+             System.out.println(result);
         int e = 20;
         int f = 4;
+             result = c * d
+             System.out.println(result);
         int g = 10;
         int h = 3;
-        System.out.println(y+z);
-        System.out.println(a-b);
-        System.out.println(c*d);
-        System.out.println(e/f);
-        System.out.println(g%h);
+             result = c * d
+             System.out.println(result);
+
+        
+        //System.out.println(y+z);
+        //System.out.println(a-b);
+        //System.out.println(c*d);
+        //System.out.println(e/f);
+        //System.out.println(g%h);
         //System.out.println("Hello, world!");
         //System.out.println("the vehicle color is " + vehicle_color);
     }
