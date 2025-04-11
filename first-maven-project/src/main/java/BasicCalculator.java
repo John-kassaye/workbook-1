fc53ac6319b3f8dec8b1fc60d46da3da98581457
Yohannes Kassaye <jmule1567@gmail.com>
2025-04-11 12:18:05 -0700

    adding possible calculations

--- first-maven-project/src/main/java/BasicCalculator.java
+++ first-maven-project/src/main/java/BasicCalculator.java
@@ -7,13 +7,19 @@ public class BasicCalculator {
         double num = input.nextDouble();
         System.out.print("Enter the second num\n");
         double num2 = input.nextDouble();
+        input.nextLine();
         System.out.println("possible calculations\nadd\nsubtract\nmultiply\ndivide");
         String option = input.nextLine();
-        System.out.println("please select an option: multiply");
+        double result=0;
+        if(option.equals("add")){
+             result= num + num2;
+        }else if(option.equals("subtract")){
+             result= num - num2;
+        }else if(option.equals("divide")){
+            result= num / num2;
+        }else if(option.equals("multiply")){
+            result= num * num2;}
 
-        int result= (int) (num * num2);
-        System.out.println("here is ur result: " + result);
-
-
-    }
+            System.out.println("here is ur result: " + result);
+        }
 }
